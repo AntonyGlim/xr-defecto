c6b941ddaf53bcb769e408e69da9e8f1d7a90c1c
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-10-02 10:04:02 +0300

    initial commit

---
+++ src/main/java/ru/asterra/xrdefecto/XrDefectoApplication.java
@@ -0,0 +1,13 @@
+package ru.asterra.xrdefecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XrDefectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XrDefectoApplication.class, args);
+	}
+
+}
